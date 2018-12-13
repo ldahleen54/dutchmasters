fc759e5bf610a214f1cd8824f84049ee3cf65a9d
Luke Dahleen <ladahle@ilstu.edu>
2018-12-13 16:03:34 -0600

    breaking the website

---
+++ server.js
@@ -0,0 +1,5 @@
+// server.js
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
---
+++ timer.js
@@ -0,0 +1,6 @@
+//timer.js
+const name = 'Slim Shady';
+ReactDOM.render(
+  <h1 className="hello">My name is {name}!</h1>,
+  document.getElementById('root')
+);
