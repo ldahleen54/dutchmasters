3a0632cb3622eede74d0df26568153fb0eef0e4d
ldahleen54 <ldahleen54@gmail.com>
2018-12-20 15:18:05 -0600

    fixed header.js with handlebars

--- js/header.js
+++ js/header.js
@@ -1,67 +1,25 @@
 var handlebars = require('handlebars');
+var fs = require('fs');
 var http = require('http');
 
-const data = {
-  title: "Dutchmasters",
-  champions: "pages/champions.html",
-  history: "pages/history.html",
-  pictures: "pages/pictures.html",
-  safety: "pages/safety.html",
-  scores: "pages/scores.html",
-  videos: "pages/videos.html",
-  whattobring: "pages/whattobring.html"
-};
+const inFile = 'header.hbs';
+const outFile = 'header.html';
 
-const source = `
-<head> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">{{title}}</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+const data = require('./info.json');
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-`;
+const source = fs.readFileSync(inFile, 'utf8');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    const template = handlebars.compile(source, { strict: true });
+
     const result = template(data);
     res.write(result);
     res.end();
-    var context = {title: "My New Post", body: "This is my first post!"};
-    var html    = template(context);
+    // var context = {title: "My New Post", body: "This is my first post!"};
+    // var html    = template(context);
 }).listen(8080);
 
 // var source   = document.getElementById("entry-template").innerHTML;
-
+const template = handlebars.compile(source, { strict: true });
 // const result = template(data);
 // console.log(result);
