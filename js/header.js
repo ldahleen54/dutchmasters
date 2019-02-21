dc949877ac17992af4cb9c4a10b63fc72f242f54
ladahle@ilstu.edu <ladahle@ilstu.edu>
2019-02-21 13:23:12 -0600

    made heaader.js a helper function

--- js/header.js
+++ js/header.js
@@ -1,17 +1,16 @@
 //This is a utility file that generates the header
 var handlebars = require('handlebars');
 var fs = require('fs');
-var http = require('http');
 
 const inFile = 'header.hbs';
-const outFile = 'header.html';
-
 const json = require('./pages.json');
 
 const source = fs.readFileSync(inFile, 'utf8');
+const template = handlebars.compile(source, { strict: true });
 module.exports = {
     headerMake: function() {
-        return "test";
+        const result = template(json);
+        return result;
     }
 }
 
--- js/server.js
+++ js/server.js
@@ -14,17 +14,14 @@ const source = fs.readFileSync(inFile, 'utf8');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    const result = template(json);
-    res.write(result);
+    
     
     let val = header.headerMake();
-    res.write(val)
+    res.write(val);
     res.end();
-    // var context = {title: "My New Post", body: "This is my first post!"};
-    // var html    = template(context);1
 }).listen(8080);
 
-// var source   = document.getElementById("entry-template").innerHTML;
-const template = handlebars.compile(source, { strict: true });
+
+
 
 
