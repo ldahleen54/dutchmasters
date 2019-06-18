9e9998d518dfcc9bce5256b376fd6775815ed3b7
ldahleen54 <ldahleen54@gmail.com>
2019-06-17 20:05:30 -0500

    fixed files to work with npm start

--- js/pagegenerator.js
+++ js/pagegenerator.js
@@ -6,63 +6,63 @@ const json = require('./pages.json');
 
 module.exports = {
     makeHeader: function() {
-        var inFile = 'header.hbs';
+        var inFile = './pages/header.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json)
         return result;
     },
     makeChampions: function() {
-        var inFile = 'champions.hbs';
+        var inFile = './pages/champions.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
         return result;
     },
     makeHistory: function() {
-        var inFile = 'history.hbs';
+        var inFile = './pages/history.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
         return result;
     },
     makePictures: function() {
-        var inFile = 'pictures.hbs';
+        var inFile = './pages/pictures.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
         return result;
     },
     makeSafety: function() {
-        var inFile = 'safety.hbs';
+        var inFile = './pages/safety.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
         return result;
     },
     makeScores: function() {
-        var inFile = 'scores.hbs';
+        var inFile = './pages/scores.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
         return result;
     },
     makeVideos: function() {
-        var inFile = 'videos.hbs';
+        var inFile = './pages/videos.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
         return result;
     },
     makeWhatToBring: function() {
-        var inFile = 'whattobring.hbs';
+        var inFile = './pages/whattobring.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
         return result;
     },
     makeIndex: function() {
-        var inFile = 'index.hbs';
+        var inFile = './pages/index.hbs';
         var source = fs.readFileSync(inFile, 'utf8');
         var template = handlebars.compile(source, { strict: true });
         var result = template(json);
