9e82480c59dbf6c88691c0dc64f036e33c468820
ldahleen54 <ldahleen54@gmail.com>
2019-07-03 17:33:05 -0500

    changed procfile to help run application

--- js/server.js
+++ js/server.js
@@ -13,44 +13,44 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log('Your node js server is running');
 });
 
-// http.createServer(function (request, response) {
-//     const { headers, method, url } = request;
-//     const userAgent = headers['user-agent'];
+http.createServer(function (request, response) {
+    const { headers, method, url } = request;
+    const userAgent = headers['user-agent'];
 
-//     var body = [];
-//     request.on('data', (chunk) => {
-//         body.push(chunk);
-//     }).on('end', () => {
-//         body = Buffer.concat(body).toString();
-//     }).on('error', (err) => {
-//         response.writeHead(404);
-//         response.write('The contents you are looking for are not found');
-//         console.error(err);
-//     });
+    var body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+    }).on('error', (err) => {
+        response.writeHead(404);
+        response.write('The contents you are looking for are not found');
+        console.error(err);
+    });
     
-//     response.writeHead(200, {'Content-Type': 'text/html'});    
-//     var nav = generator.makeHeader();
-//     response.write(nav);
-//     if(url === "/index") {
-//         response.write(generator.makeIndex());
-//     } else if( url === "/champions") {
-//         response.write(generator.makeChampions());
-//     } else if( url === "/history") {
-//         response.write(generator.makeHistory());
-//     } else if( url === "/pictures") {
-//         response.write(generator.makePictures());
-//     } else if( url === "/safety") {
-//         response.write(generator.makeSafety());
-//     } else if( url === "/scores") {
-//         response.write(generator.makeScores());
-//     } else if( url === "/videos") {
-//         response.write(generator.makeVideos());
-//     } else if( url === "/whattobring") {
-//         response.write(generator.makeWhatToBring());
-//     }
-//     response.end();
-// }).listen(8080);
+    response.writeHead(200, {'Content-Type': 'text/html'});    
+    var nav = generator.makeHeader();
+    response.write(nav);
+    if(url === "/index" || url === "") {
+        response.write(generator.makeIndex());
+    } else if( url === "/champions") {
+        response.write(generator.makeChampions());
+    } else if( url === "/history") {
+        response.write(generator.makeHistory());
+    } else if( url === "/pictures") {
+        response.write(generator.makePictures());
+    } else if( url === "/safety") {
+        response.write(generator.makeSafety());
+    } else if( url === "/scores") {
+        response.write(generator.makeScores());
+    } else if( url === "/videos") {
+        response.write(generator.makeVideos());
+    } else if( url === "/whattobring") {
+        response.write(generator.makeWhatToBring());
+    }
+    response.end();
+}).listen(8080);
