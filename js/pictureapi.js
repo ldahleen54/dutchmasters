a16dd16f573a680c16328826c1c8550504b81afb
Luke Dahleen <ldahleen54@gmail.com>
2020-05-31 03:52:38 -0500

    Picture api redirects to s3 when using the picture api

--- js/pictureapi.js
+++ js/pictureapi.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 // const inFile = 'header.hbs';
 
 const data = require('./pages.json');
-
 // const source = fs.readFileSync(inFile, 'utf8');
 
 // var appDir = global.rootDir;
@@ -16,18 +15,17 @@ var imageDir = "https://dutchmasters.s3.us-east-2.amazonaws.com/";
 var imageSuffix = ".jpg";
 
 //to retrieve image use /images/(name)
-
 app.get("/images/:id/:year", function (request, response) {
     console.log("params: " + request.params["year"]);
     var path = imageDir + request.params.year + "/" + request.params.id + imageSuffix;
     console.log("fetching image: ", path);
-    response.sendFile(path, { root: appDir });
+    response.redirect(path);
 });
 app.get("/images/:id", function (request, response) {
     console.log("params: " + request.params["id"]);
     var path = imageDir + request.params.id + imageSuffix;
     console.log("fetching image: ", path);
-    response.sendFile(path, { root: appDir });
+    response.redirect(path);
 });
 app.listen(5000);
 // const template = handlebars.compile(source, { strict: true });
