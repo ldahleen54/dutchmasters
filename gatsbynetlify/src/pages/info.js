88d92828a7c957019b32ce002598a36d5d62253d
ldahleen54 <ldahleen54@gmail.com>
2021-11-18 23:32:56 -0600

    Migrated safety and what to bring

---
+++ gatsbynetlify/src/pages/info.js
@@ -0,0 +1,50 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Info = () => (
+  <Layout>
+    <Seo title="Pictures" />
+    <h1>Gun Safety</h1>
+    <ol>
+      <li>Treat <strong>all</strong> guns as if they're loaded.</li>
+      <li>Never point a firearm at anything you are not willing to <strong>kill</strong> or <strong>destroy</strong>.</li>
+      <li>Keep your finger <strong>off</strong> the trigger until you are ready to shoot.</li>
+      <li>Be aware of your target and what is beyond.</li>
+      <li>Think safe be safe and be your neighbors keeper</li>
+    </ol>
+
+    <h1>
+    <strong>
+        What to Bring
+    </strong>
+    </h1>
+    <ul>
+      <li>$50 entry fee</li>
+      <li>1 Box of all-orange clay pigeons</li>
+      <li>Please avoid white flyers as they don't break as well as champion</li>
+      <li>3 boxes of shotgun shells</li>
+      <li>Additional clay pigeons &amp; shells for whatever you need for after the tournament</li>
+      <li>Please contact Rich if you want to participate in the group breakfast on Sunday </li>
+    </ul>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+  </Layout>
+)
+export default Info
