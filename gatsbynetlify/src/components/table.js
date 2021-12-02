8f79fba0d0739b147b34111808622da1cfb2c7f3
ldahleen54 <ldahleen54@gmail.com>
2021-12-01 23:30:50 -0600

    More testing

--- gatsbynetlify/src/components/table.js
+++ gatsbynetlify/src/components/table.js
@@ -1,15 +1,22 @@
 import React from "react"
-const Table = ({ ...props }) => {
-    // let headers = props.headers.map((header) => {
-    //     <th>{header}</th>
-    // });
-        return (
-            <div className="table">
-                <table>
-                    <p>testing55</p>
-                </table>
-            </div>
-        );
-    }
+const Table = ({ ...props }) => (
+<div className="table">
+                 <table>
+                     <p>testing55</p>
+                 </table>
+             </div>
+);
+// {
+//     // let headers = props.headers.map((header) => {
+//     //     <th>{header}</th>
+//     // });
+//         return (
+//             <div className="table">
+//                 <table>
+//                     <p>testing55</p>
+//                 </table>
+//             </div>
+//         );
+//     }
 
 export default Table;
