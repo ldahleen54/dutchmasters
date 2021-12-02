e4b4f977314640ba627fa3e1f7d583852865c168
ldahleen54 <ldahleen54@gmail.com>
2021-12-01 22:44:04 -0600

    Changed to use props instead of attribute directly

--- gatsbynetlify/src/components/table.js
+++ gatsbynetlify/src/components/table.js
@@ -1,12 +1,12 @@
 import React from "react"
-const Table = ({ headers, data, ...props }) => {
-    const listHeaders = headers.map((header) => {
+const Table = ({ ...props }) => {
+    const headers = props.headers.map((header) => {
         <th>{header}</th>
     });
         return (
             <div className="table">
                 <table>
-                    {listHeaders}
+                    {headers}
                 </table>
             </div>
         );
