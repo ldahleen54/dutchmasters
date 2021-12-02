5ff60c5a194c77ecebbca4d57ca3ba3f0aaf5c24
ldahleen54 <ldahleen54@gmail.com>
2021-12-01 22:18:24 -0600

    Changed width and height of videos

--- gatsbynetlify/src/components/video.js
+++ gatsbynetlify/src/components/video.js
@@ -4,6 +4,8 @@ const Video = ({ videoSrcURL, videoTitle, ...props }) => (
     <iframe
       src={videoSrcURL}
       title={videoTitle}
+      width="100%"
+      height="100%"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       frameBorder="0"
       webkitallowfullscreen="true"
