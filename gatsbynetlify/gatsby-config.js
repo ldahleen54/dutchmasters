6e344ddeb806b02318b8d95481abeea2e4d8c8cb
ldahleen54 <ldahleen54@gmail.com>
2022-01-27 23:20:01 -0600

    Fixed gatsby build

--- gatsbynetlify/gatsby-config.js
+++ gatsbynetlify/gatsby-config.js
@@ -30,7 +30,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -38,8 +37,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- gatsbynetlify/src/pages/index.js
+++ gatsbynetlify/src/pages/index.js
@@ -1,14 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <StaticImage 
+    <image
       src="https://dutchmasters.s3.us-east-2.amazonaws.com/123_1.jpg"
       alt="Dutchmasters Welcome Picture"
     />
