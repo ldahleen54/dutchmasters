07adf6460c7bf779382ae54a93c9fb361cd9b610
ldahleen54 <ldahleen54@gmail.com>
2022-06-20 23:49:59 -0500

    Disabled header unit test

--- gatsbynetlify/jest.config.js
+++ gatsbynetlify/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
       "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
       "^gatsby-core-utils/(.*)$": `gatsby-core-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
     },
-    testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+    testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`, `header.test.js`],
     transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
     globals: {
       __PATH_PREFIX__: ``,
