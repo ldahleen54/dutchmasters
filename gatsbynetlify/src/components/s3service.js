09ec4cc2df7a95ee0293af8e5f1624d46c6f4f55
ldahleen54 <ldahleen54@gmail.com>
2022-07-05 22:10:05 -0500

    Added s3service to gatsby directory

---
+++ gatsbynetlify/src/components/s3service.js
@@ -0,0 +1,29 @@
+var params = {
+    Bucket: "dutchmasters"
+};
+process.env.AWS_PROFILE = 'default';
+var AWS = require('aws-sdk');
+var fs = require('fs');
+var credentials = new AWS.SharedIniFileCredentials();
+AWS.config.credentials = credentials;
+var s3  = new AWS.S3();
+
+// updates the s3objects json
+module.exports = {
+    getPictures: () => {
+        s3.listObjectsV2(params, function(error, data) {
+            if(error) {
+                console.log(error, error.stack);
+            } else {
+                // doesn't change the global variable
+                fs.writeFile('s3objects.json', JSON.stringify(data), function (error) {
+                    if (error) throw error;
+                    console.log('Retrieved list of objects. Saved to s3 ');
+                });
+            }
+        });
+    }
+}
+
+
+
