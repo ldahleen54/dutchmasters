c623f8bfaa0b4ea4a37e660a8696fbc652cea8dd
ldahleen54 <ldahleen54@gmail.com>
2022-07-05 23:00:12 -0500

    Added json files for pictures, added every year as individual retrieval for s3

--- gatsbynetlify/src/data/s3service.js
+++ gatsbynetlify/src/data/s3service.js
@@ -1,6 +1,13 @@
-var params = {
-    Bucket: "dutchmasters"
+let params = {
+    Bucket: "dutchmasters",
+    Prefix: "2009/"
 };
+const years = [
+    "2006",
+    "2008",
+    "2009",
+    "2019"
+]
 process.env.AWS_PROFILE = 'default';
 var AWS = require('aws-sdk');
 var fs = require('fs');
@@ -8,14 +15,18 @@ var credentials = new AWS.SharedIniFileCredentials();
 AWS.config.credentials = credentials;
 var s3  = new AWS.S3();
 
-s3.listObjectsV2(params, function(error, data) {
-    if(error) {
-        console.log(error, error.stack);
-    } else {
-        // doesn't change the global variable
-        fs.writeFile(`${__dirname}/s3objects.json`, JSON.stringify(data), function (error) {
-            if (error) throw error;
-            console.log('Retrieved list of objects. Saved to s3objects.json.');
-        });
-    }
-});
+for(let i = 0; i < years.length; i++) {
+    params.Prefix = years[i] + "/";
+    s3.listObjectsV2(params, function(error, data) {
+        if(error) {
+            console.log(error, error.stack);
+        } else {
+            fs.writeFile(`${__dirname}/${years[i]}s3objects.json`, JSON.stringify(data), function (error) {
+                if (error) throw error;
+                console.log(`Retrieved list of objects. Saved to ${years[i]}s3objects.json.`);
+            });
+        }
+    });
+}
+
+
