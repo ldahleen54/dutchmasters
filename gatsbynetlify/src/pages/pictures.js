51c32c95aabc0fe941efcaf4a2438ba09f16b0ed
ldahleen54 <ldahleen54@gmail.com>
2022-07-08 00:00:48 -0500

    Added 2006 picture list to the pictures page

--- gatsbynetlify/src/pages/pictures.js
+++ gatsbynetlify/src/pages/pictures.js
@@ -3,20 +3,32 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as objects2006 from "../data/2006s3objects.json"
 
-const Pictures = () => (
-  <Layout>
-    <Seo title="Pictures" />
-    <nav aria-label="Year navigation">
-    <ul class="pagination">
-      <li class="page-item"><button class="btn active">Previous</button></li>
-      <li class="page-item active "><button class="btn active">2006</button></li>
-      <li class="page-item"><button class="btn">2008</button></li>
-      <li class="page-item"><button class="btn">2009</button></li>
-      <li class="page-item"><button class="btn">Next</button></li>
-    </ul>
-    </nav>
-    <Link to="/">Back to Home Page</Link>
-  </Layout>
-)
+const Pictures = () => {
+  let pictures2006 = objects2006.objects;
+  return (
+    <Layout>
+      <Seo title="Pictures" />
+      <nav aria-label="Year navigation">
+      <ul class="pagination">
+        <li class="page-item"><button class="btn active">Previous</button></li>
+        <li class="page-item active "><button class="btn active">2006</button></li>
+        <li class="page-item"><button class="btn">2008</button></li>
+        <li class="page-item"><button class="btn">2009</button></li>
+        <li class="page-item"><button class="btn">2019</button></li>
+        <li class="page-item"><button class="btn">Next</button></li>
+      </ul>
+      </nav>
+    <ul className="list-group">
+        {pictures2006.map(path => (
+          <img className="list-group-item list-group-item-primary" src={"https://dutchmasters.s3.us-east-2.amazonaws.com/" + path}/>
+        ))}
+      </ul>
+      <Link to="/">Back to Home Page</Link>
+    </Layout>
+      
+  );
+  
+}
 export default Pictures
