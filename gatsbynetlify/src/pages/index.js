cb45e2f7ffe7fb4132e52094c35920b09fab3b9d
Luke Dahleen <ldahleen54@gmail.com>
2025-02-09 17:51:04 -0600

    Add image gallery to front page

--- gatsbynetlify/src/pages/index.js
+++ gatsbynetlify/src/pages/index.js
@@ -2,16 +2,23 @@ import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import ImageGallery from "react-image-gallery";
+
+const images = [
+  {
+    original: "https://dutchmasters.s3.us-east-2.amazonaws.com/2019/elder_group.jpg"
+  },
+  {
+    original: "https://dutchmasters.s3.us-east-2.amazonaws.com/2019/younggroupphoto.jpg"
+  }
+]
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <img
-      src="https://dutchmasters.s3.us-east-2.amazonaws.com/2019/elder_group.jpg"
-      alt="Dutchmasters Welcome Picture"
-    />
+    <ImageGallery items={images} />
     <p>
-      <Link to="/pictures">See More Pictures</Link>
+      <Link to="/pictures">See More Pictursaes</Link>
     </p>
   </Layout>
 )
